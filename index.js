64a5354356a7136c7e75976bc0e67de7132535e1
Duynn250390 <51281262+duynn250390@users.noreply.github.com>
2019-10-17 23:43:17 +0700

    tai len lan dau

---
+++ index.js
@@ -0,0 +1,27 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res) {
+    // res.send('<h1>Xin Chao</h1>');
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+    console.log('Có một kết nối');
+    socket.on('disconnect', function() {
+        console.log('Có một kết nối đã ngắt');
+    });
+});
+
+io.on('connection', function(socket) {
+    socket.on('chat-mesage', function(msg) {
+        console.log(msg);
+        io.emit('chat-mesage', msg)
+    });
+})
+
+http.listen(3000, function() {
+    console.log('Listening on *:' + port);
+});
