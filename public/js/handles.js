5a2caade6ae965ff68c7cf00390ae57f5de10794
Duynn250390 <51281262+duynn250390@users.noreply.github.com>
2019-10-18 12:58:18 +0700

    dfdf

--- public/js/handles.js
+++ public/js/handles.js
@@ -1,6 +1,6 @@
-$(function () {
+$(function() {
     var socket = io();
-    $('form').submit(function (e) {
+    $('form').submit(function(e) {
         e.preventDefault();
         if ($('#mes').val() != '') {
             socket.emit('chat-mesage-to-client', $('#mes').val());
@@ -8,28 +8,37 @@ $(function () {
             return false;
         }
     });
-    socket.on('chat-mesage-to-server', function (data) {
+    socket.on('chat-mesage-to-server', function(data) {
         $('#messages').append($('<li>').text(data));
         window.scrollTo(0, document.body.scrollHeight);
     });
 });
 
-$(document).ready(function () {
+$(document).ready(function() {
     var socket = io();
     $('.person_list').show();
     $('.box_chat').hide();
-    $('#formID_regis').submit(function (e) {
+    $('#formID_regis').submit(function(e) {
         var username = $('#input_message').val();
         if (username != '') {
             socket.emit('client-regis-username', username);
         }
     });
-    socket.on('regis-user-that-bai', function () {
+    $('.hello_mem_form').submit(function(e) {
+        socket.emit('client-logout');
+    });
+    socket.on('regis-user-that-bai', function() {
         $('#input_message').addClass('error');
     });
-    socket.on('regis-user-thanh-cong', function (data) {
+    socket.on('regis-user-thanh-cong', function(data) {
         $('.name_hello span').html(data);
         $('.box_chat').show();
         $('.form_regis').hide(500);
+    });
+    socket.on('update-list-users', function(data) {
+        $('#list_mem').html('');
+        data.forEach(function(i) {
+            $('#list_mem').append("<li clas='userOnline'>" + i + "</li>");
+        });
     });
 });
--- server.js
+++ server.js
@@ -11,26 +11,26 @@ app.set("view engine", "ejs");
 app.set("views", "./views");
 
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.render('trangchu');
 });
-app.get('/bai1', function (req, res) {
+app.get('/bai1', function(req, res) {
     res.render('bai1');
 });
 var danhsachUsers = [];
 
-io.on('connection', function (socket) {
+io.on('connection', function(socket) {
     console.log('Có một kết nối:' + socket.id);
-    socket.on('disconnect', function () {
+    socket.on('disconnect', function() {
         console.log(socket.id + 'đã ngắt kết nối');
     });
-    socket.on('chat-mesage-to-client', function (data) {
+    socket.on('chat-mesage-to-client', function(data) {
         // console.log();
         // socket.emit('chat-mesage-to-server', data); // A gửi lên và SV chỉ gửi về A
         // socket.broadcast.emit('chat-mesage-to-server', data);// Chat đến người khác nhưng không gửi về mình
         io.sockets.emit('chat-mesage-to-server', data);
     });
-    socket.on('client-regis-username', function (data) {
+    socket.on('client-regis-username', function(data) {
         if (danhsachUsers.indexOf(data) >= 0) {
             socket.emit('regis-user-that-bai');
         } else {
@@ -38,9 +38,13 @@ io.on('connection', function (socket) {
             danhsachUsers.push(data);
             socket.Username = data;
             socket.emit('regis-user-thanh-cong', data);
+            io.sockets.emit('update-list-users', danhsachUsers);
         }
     });
-
+    socket.on('client-logout', function() {
+        danhsachUsers.splice(socket.Username);
+        io.sockets.emit('update-list-users', danhsachUsers);
+    });
 
 
 
