6fa9461bd6194c60c7eb093d8001b93184ba4094
unknown <duynn250390@gmail.com>
2019-10-18 16:02:40 +0700

    dsd

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// socket.emit('chat-mesage-to-server', data); // A gửi lên và SV chỉ gửi về A
+// socket.broadcast.emit('chat-mesage-to-server', data);// Chat đến người khác nhưng không gửi về mình
 var express = require('express');
 var app = express();
 
@@ -11,41 +13,46 @@ app.set("view engine", "ejs");
 app.set("views", "./views");
 
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.render('trangchu');
 });
-app.get('/bai1', function(req, res) {
+app.get('/bai1', function (req, res) {
     res.render('bai1');
 });
 var danhsachUsers = [];
-
-io.on('connection', function(socket) {
-    console.log('Có một kết nối:' + socket.id);
-    socket.on('disconnect', function() {
+var listUser= {};
+io.on('connection', function (socket) {
+    // console.log('Có một kết nối:' + socket.id);
+    socket.on('disconnect', function () {
         console.log(socket.id + 'đã ngắt kết nối');
     });
-    socket.on('chat-mesage-to-client', function(data) {
-        // console.log();
-        // socket.emit('chat-mesage-to-server', data); // A gửi lên và SV chỉ gửi về A
-        // socket.broadcast.emit('chat-mesage-to-server', data);// Chat đến người khác nhưng không gửi về mình
-        io.sockets.emit('chat-mesage-to-server', data);
-    });
-    socket.on('client-regis-username', function(data) {
+    socket.on('client-regis-username', function (data) {
         if (danhsachUsers.indexOf(data) >= 0) {
             socket.emit('regis-user-that-bai');
         } else {
-            // SUSSCES
+           
             danhsachUsers.push(data);
-            socket.Username = data;
+            // danhsachUsers[socket.id].username = data;
+            // socket.Username = data;
+            socket.nickname = data;
+            // listUser.Usernames = danhsachUsers;
+            // listUser.currentUser = data;
+            // socket.Gender = 'Nam';
+            // console.log(listUser);
+            
             socket.emit('regis-user-thanh-cong', data);
             io.sockets.emit('update-list-users', danhsachUsers);
         }
     });
-    socket.on('client-logout', function() {
-        danhsachUsers.splice(socket.Username);
-        io.sockets.emit('update-list-users', danhsachUsers);
+    socket.on('client-logout', function () {
+        danhsachUsers.splice(danhsachUsers.indexOf(socket.Username), 1);
+        socket.broadcast.emit('update-list-users', danhsachUsers);
+        socket.emit('client-logout-thanh-cong');
     });
-
+    socket.on('chat-mesage-to-client', function (data) {
+        io.sockets.emit('chat-mesage-to-server-chat', { ten: socket.nickname, nd: data });
+    });
+    console.log(socket.nickname)
 
 
 });
